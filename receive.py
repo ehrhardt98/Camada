7f8c9df43d4e631a8422b33f25663c0268dac16d
ehrhardt98 <jorgeavbe@al.insper.edu.br>
2018-10-18 14:45:46 -0300

    receive

--- receive.py
+++ receive.py
@@ -4,10 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import peakutils
 import keyboard
+from peakutils.plot import plot as pplot
 
 fs = 44100
 duration = 1
+channel = 2
 sd.default.samplerate = fs
+sd.default.channels = channel
 
 signal = Signal()
 
@@ -15,16 +18,19 @@ recording = True
 
 while recording:
 
-	myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=1)
+	myrecording = sd.rec(int(duration * fs))
 
 	signals = []
 
 	for i in myrecording:
 		signals.append(i[0])
 
-	x, y = signal.plotFFT(signals, fs)
+	x, y = signal.calcFFT(signals, fs)
 
-	indexes = peakutils.indexes(y, thres=0.3, min_dist=100)
+	indexes = peakutils.indexes(y, thres=0.6, min_dist=100)
+
+	pplot(x,y,indexes)
+
 	if indexes != []:
 		print(indexes)
 
